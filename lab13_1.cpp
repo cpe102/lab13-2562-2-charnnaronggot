46471247c2bec5c67aabdd2e45d2caa2ee9a0092
charnnaronggot <charnnarong_2542@hotmail.com>
2020-02-25 11:50:03 +0700

    LAB13

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -11,11 +11,38 @@ void swap(T d[],int x,int y){
 
 template <typename T>
 void insertionSort(T d[],int N){
-	
-//Write your code here.
+	int l ;
+	T max ;
+		for (int i = 0 ; i < N ;i++ ){
+			max = d[i] ;
+			if (max < d[i])
+			d[i] = max ;
+			l = i ;
+			for(int k = 0 ; k < N ; k++){
+				if ( k == l ){
+					cout <<'['<< d[k] <<']';
+				}
+				else cout << d[k] << " " ;
 
+			}
+
+	for (int j = i-1 ; j >= 0 ; j--) {
+		if (max > d[j]){
+			swap(d , j , l) ;
+			l = j ;
+		}else break ;
+	}
+	cout << " =>  " ;	
+
+	for(int k = 0 ; k < N ; k++){
+		if(k == l){
+			cout <<"["<<d[k]<<"]";
+		}else cout <<d[k]<< " " ;
+	}
+	cout << endl ;
+
+		}
 }
-
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
 	cout << "Input Array:";
